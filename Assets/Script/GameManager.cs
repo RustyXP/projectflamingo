7c985b8fb624c39c24c34c63216c15e9c1e39b71
Workstation admin (ISO1) account a1GreLud <a1GreLud@lego.com>
2020-02-01 15:52:23 -0500

    Adding GameManager

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Make the Game Manager a singleton, so we can access it across
+    // all scripts and scenes.
+    public static GameManager instance = null;
+    public int level = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        Debug.Log("Hello!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
