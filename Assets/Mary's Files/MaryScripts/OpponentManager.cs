a7353684c4a875781ab9bfc5c3460a9789f19e86
Mary <ml4372@nyu.edu>
2020-02-01 15:41:55 -0500

    play scene works

--- Assets/Mary's Files/MaryScripts/OpponentManager.cs
+++ Assets/Mary's Files/MaryScripts/OpponentManager.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ Purpose: Holds the data about the opponent 
+            (body sprite, face sprites, face position (if perspective is used), dialogue, emotional state)
+ 
+ 
+ Usage:    
+  
+ */
 public class OpponentManager : MonoBehaviour
 {
+    
     public string name;
 
     public int id;
@@ -13,10 +21,26 @@ public class OpponentManager : MonoBehaviour
     public Sprite bodySprite;
 
     public Sprite[] emotions;
+
+    private int _currentEmotion;
+
+    public string[] dialogue;
+    public ArrayList splitDialogue;
+
+    public int currentEmotion
+    {
+        get { return _currentEmotion; }
+        set
+        {
+            faceSR.sprite = emotions[value];
+            _currentEmotion = value;
+        }
+    }
     
     // Start is called before the first frame update
     void Start()
     {
+        
         
     }
 
@@ -25,4 +49,6 @@ public class OpponentManager : MonoBehaviour
     {
         
     }
+    
+    
 }
--- Assets/Mary's Files/MaryScripts/PlaySceneManager.cs
+++ Assets/Mary's Files/MaryScripts/PlaySceneManager.cs
@@ -1,10 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/*
+ Purpose:
+ 
+ Usage: 
+ */
+
 
 public class PlaySceneManager : MonoBehaviour
 {
-    public static int 
+    private int _currentState;
+    public int currentState
+    {
+        get { return _currentState; }
+        set
+        {
+            _currentState = value;
+            
+        }
+    }
+
+    public Text OpponentSpeech;
+    public bool opponentTalking;
+    
+    WaitForSeconds waitDialogue = new WaitForSeconds(0.2f);
+    public 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +39,25 @@ public class PlaySceneManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void sceneEnder()
+    {
+        
+    }
+
+    IEnumerator dialogueDisplayer(string splitDialogue)
+    {
+        string sr = "";
+        string[] characters = new string[splitDialogue.Length];
+        opponentTalking = true;
+        for (int i = 0; i < splitDialogue.Length; i++)
+        {
+            sr += splitDialogue[i];
+            OpponentSpeech.text = sr;
+            yield return waitDialogue;
+        }
+
+        opponentTalking = false;
     }
 }
