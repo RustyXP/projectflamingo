bfe5206630b517e13c9f4a7dfd1c82054eb7f7cd
Mary <ml4372@nyu.edu>
2020-02-01 13:41:26 -0500

    1 script: testerScript

---
+++ Assets/testerScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testerscript : MonoBehaviour
+{
+    public PlayerEmotions PE;
+
+    //0-happy, 1-sad, 2-angry, 3-surprise,
+    public Sprite[] emotions;
+    public SpriteRenderer NPCface;
+
+    public int currEmotion = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (currEmotion < emotions.Length)
+            {
+                currEmotion++;
+            }
+            else
+            {
+                currEmotion = 0;
+            }
+
+            
+        }
+        NPCface.sprite = emotions[currEmotion];
+        //use player face to change curr emotion
+        if (Input.GetKey(KeyCode.K))
+        {
+            updateFace();
+        }
+        else
+        {
+            currEmotion = 1;
+        }
+    }
+
+    void updateFace()
+    {
+        if (currEmotion == 1)
+        {
+            Debug.Log(PE.currentJoy);
+            if (PE.currentJoy > 50f)
+            {
+                Debug.Log("change emotions");
+                currEmotion = 0;
+            }
+        }
+    }
+}
